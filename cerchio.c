e336f51217a93ff673eb86483f600093cbbece67
Antonio <ciao@gmail.com>
2017-02-24 09:13:03 +0100

    cerchio

--- cerchio.c
+++ cerchio.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 int main(){
-int r, c, s;
+int r;
+double c, s;
 double p=3.14;
 printf("Inserisci il valore del raggio: ");
 scanf("%d", &r);
 c= 2.0*p*r;
 s= (r*r)*p;
 
-printf("La circonferenza di un cerchio avente come raggio %d= %d\n",r,c);
-printf("La superficie di un cerchio avente come raggio %d= %d\n", r,s);
+printf("La circonferenza di un cerchio avente come raggio %d= %.2f\n",r,c);
+printf("La superficie di un cerchio avente come raggio %d= %.2f\n", r,s);
 
 return 0;
 }
